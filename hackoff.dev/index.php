ec2436c5bbb1674a92b0f7f045d6cf20fd62182c
hackoffme <flaton.88@mail.ru>
2016-10-25 11:54:28 +0800

    dev

--- hackoff.dev/index.php
+++ hackoff.dev/index.php
@@ -9,36 +9,52 @@
         <link rel="stylesheet" type="text/css" href="src/jquery.jqplot.css" />
     </head>
     <body>
-        
+
         <?php
-        $valuteID='R01235';
-        $valuteName['R01235']='USD';
-        $valuteName['R01239']='EURO';
-        if (isset($_POST['button'])){
-            $valuteID=$_POST['selectValute'];
+        $valuteID = 'R01235';
+        $valuteName['R01235'] = 'USD';
+        $valuteName['R01239'] = 'EURO';
+        $countDayinFile = 0;
+        $valuteFile = [];
+        if (isset($_POST['button'])) {
+            $valuteID = $_POST['selectValute'];
         }
         $Url = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=";
-        for ($i = 0; $i < 10; $i++) {
+        if (file_exists('data')) {
+            $file = file_get_contents('data');
+            $valuteFile = unserialize($file);
+            if (array_key_exists($valuteID, $valuteFile)) {
+                $valuteFileThis = $valuteFile[$valuteID];
+                if (isset($valuteFileThis)) {
+                    $countDayinFile = ceil((time() - strtotime($valuteFileThis[count($valuteFileThis) - 1][0])) / 3600 / 24);
+                    //print_r($valuteFileThis);
+                    if ($countDayinFile > 0) {
+                        $valuteList = array_slice($valuteFileThis, 0, $countDayinFile);
+                    }
+                }
+            }
+        }
+
+        for ($i = 0; $i < 10 - $countDayinFile; $i++) {
             $date = date('d/m/Y', mktime(0, 0, 0, date('m'), date('d') - $i, date('Y')));
             $dateForJava = date('Y-m-d', mktime(0, 0, 0, date('m'), date('d') - $i, date('Y')));
-            $filename='/opt/lampp/docs/hackoff.dev/'.$dateForJava.$valuteName[$valuteID];
-            $UrlDate = $Url . $date ;
-            if (file_exists($filename)) {
-                $xml = simplexml_load_file($filename);
-            } else {
-               $xml = simplexml_load_file($UrlDate); 
-               $xml->asXML($filename);
-            }
-            $valute = $xml->xpath(".//*[@ID='".$valuteID."']");
+            $UrlDate = $Url . $date;
+            $xml = simplexml_load_file($UrlDate);
+            $valute = $xml->xpath(".//*[@ID='" . $valuteID . "']");
             $valuteList[] = [$dateForJava, floatval(str_replace(',', '.', $valute[0]->Value))];
         }
+        $valuteFile[$valuteID] = $valuteList;
+        $serializeValute = serialize($valuteFile);
+        $f = fopen('data', 'w');
+        fwrite($f, $serializeValute);
+        fclose($f);
         ?>   
 
         <div id='plot' style="width:500px;height:400px"></div>
         <script>
             var ar = <?php echo json_encode($valuteList) ?>;
             var plot1 = $.jqplot('plot', [ar], {
-                title: <?php echo json_encode($valuteName[$valuteID]);?>,
+                title: <?php echo json_encode($valuteName[$valuteID]); ?>,
                 axes: {
                     xaxis: {
                         renderer: $.jqplot.DateAxisRenderer,
@@ -61,7 +77,7 @@
                 }
             });
         </script>
-        <form action="index.php" method="post" name='form'>
+        <form action=<?php echo $_SERVER['SCRIPT_NAME']; ?> method="post" name='form'>
             <select name='selectValute'>
                 <option  value="R01235">USD</option>
                 <option value="R01239">EURO</option>
